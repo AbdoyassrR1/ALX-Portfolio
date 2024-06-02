f7518f1c52d950f5c648e2e288cb4566a3e7f99a
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-06-02 17:33:21 +0300

    ready for landing page

--- components/No-Tasks.js
+++
@@ -1,8 +0,0 @@
-export const noTasks = (element) => {
-  element.innerHTML = `
-        <div class="no-tasks animate__animated animate__wobble">
-            <i class="bi bi-emoji-frown-fill"></i>
-            <h3>No tasks found</h3>
-        </div>
-    `;
-};
